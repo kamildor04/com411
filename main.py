3cdfa87bfcd950463deaa9e5fda653cc0e4667e8
Kamil Dorosz <6dorok91@solent.ac.uk>
2022-10-14 15:14:59 +0100

    Testing PyCharm with GitHub.

---
+++ main.py
@@ -0,0 +1,16 @@
+# Activity 1
+book = input("What type of book is this?")
+
+if book == "Adventure":
+    print("I like adventure books")
+print("Finished reading book.")
+
+# Activity 2
+
+activity = input("Please enter the activity to be performed.")
+
+if activity == "calculate":
+    print("Performing calculations...")
+print("Activity completed!")
+
+# Activity 3
